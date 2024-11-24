86cf3aa5fe3ad962c6eec590bcf3fe753f1c885c
Mattgasan <mathgabriel@outlook.com.br>
2024-11-24 16:55:18 -0300

    Arquivos

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMovementController 
+{
+    void MoveHorizontally(float x);
+    void MoveVertically(float y);
+}
